c3f3fc52efb0db9ec6489e5ac3574bd6d62c287b
Parr <tedblount01@gmail.com>
2020-03-28 14:11:23 -0500

    api changes

--- 0-Project-Starter/Project/public/js/api.js
+++ 0-Project-Starter/Project/public/js/api.js
@@ -13,12 +13,50 @@ req.headers({
 
 
 req.end(function (covidData) {
-	if (covidData.error) throw new Error(covidData.error);
+	if (covidData.error) throw new Error(covidData.error); {
+		
+	var covidStats = covidData.body.data.covid19Stats
 
-	console.log(covidData.body.data);
-	var covidStats = covidData.body.data
-	module.exports = covidStats;
-});
+	let minnStats = covidStats.filter(stat => stat.province === 'Minnesota')
 
+	console.table(minnStats)
+
+	// function isCherries(covidStats) { 
+	// 	return fruit.name === 'cherries';
+	//   }
+	  
+	//   console.log(inventory.find(isCherries));}
+
+	// const result = covidStats.find( ({ province }) => province === 'Minneapolis' );
+
+	// console.log(result)
+
+	// covidStats.find(function(covidStat) {
+	// 		if (covidStats.province === 'Minnesota') {
+	// 		return covidStat
+	// 		}console.table(covidStat); 
+	//   });
+
+	// covidStats.forEach(function(covidStat) {
+	// 	if (covidStat.province === 'Minnesota') {
+	// 		return covidStat
+	// 	}console.table(covidStat)
+	// })
+		
+
+	// for (var i = 0; i < covidStats.length; i++) {
+
+		
+	
+	// 	if (info[i].province === "Minnesota") {
+	// 		return info[i];
+    // }console.log(info[i])
+	// 		//console.log(info)
+	// 	}
+	
+	//console.log(covidData.body.data);
+	}
+module.exports = covidStats;
+})
 
 
