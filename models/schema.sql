917d22c89744acd6b59eb40c2223ec9580775519
RisenFenix <jjstram@gmail.com>
2020-03-28 11:03:13 -0500

    second iteration of the sqltable

--- models/schema.sql
+++ models/schema.sql
@@ -2,7 +2,13 @@ DROP DATABASE IF EXISTS covid19db;
 CREATE DATABASE covid19db;
 
 CREATE TABLE need_help_tb (
-id INT NOT NULL AUTO_INCREMENT,
-name VARCHAR(225),
-
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(225),
+description VARCHAR(255),
+street_address VARCHAR(255),
+city VARCHAR (255),
+state VARCHAR (255),
+first_name VARCHAR(255),
+email VARCHAR(255),
+price decimal(8,2)
 );
