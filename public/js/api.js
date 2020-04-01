719bd031491d6914c3c62d32fcd127a831259513
Parr <tedblount01@gmail.com>
2020-03-31 21:07:15 -0500

    interactive map

--- public/js/api.js
+++ public/js/api.js
@@ -1,59 +1,31 @@
 var unirest = require("unirest");
+var mapdata = require('./mapdata')
 
-var req = unirest(
-  "GET",
-  "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats"
-);
+var req = unirest("GET", "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats");
 
 req.query({
-  country: "US"
+	"country": "US"
 });
 
 req.headers({
-  "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
-  "x-rapidapi-key": "2e6a00a0b7mshb40a079e7a67f38p181597jsn5910590141c3"
+	"x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
+	"x-rapidapi-key": "2e6a00a0b7mshb40a079e7a67f38p181597jsn5910590141c3"
 });
 
-req.end(function(covidData) {
-  if (covidData.error) throw new Error(covidData.error);
-  {
-    var covidStats = covidData.body.data.covid19Stats;
 
-    let minnStats = covidStats.filter(stat => stat.province === "Minnesota");
+req.end(function (covidData) {
+	if (covidData.error) {
+		 throw new Error(covidData.error)
+	 } else {
+		
+	var covidStats = covidData.body.data.covid19Stats
 
-    console.table(minnStats);
+	var minnStats = covidStats.filter(stat => stat.province === 'Minnesota')
+	//module.exports = minnStats
 
-    // function isCherries(covidStats) {
-    // 	return fruit.name === 'cherries';
-    //   }
-
-    //   console.log(inventory.find(isCherries));}
-
-    // const result = covidStats.find( ({ province }) => province === 'Minneapolis' );
-
-    // console.log(result)
-
-    // covidStats.find(function(covidStat) {
-    // 		if (covidStats.province === 'Minnesota') {
-    // 		return covidStat
-    // 		}console.table(covidStat);
-    //   });
-
-    // covidStats.forEach(function(covidStat) {
-    // 	if (covidStat.province === 'Minnesota') {
-    // 		return covidStat
-    // 	}console.table(covidStat)
-    // })
-
-    // for (var i = 0; i < covidStats.length; i++) {
-
-    // 	if (info[i].province === "Minnesota") {
-    // 		return info[i];
-    // }console.log(info[i])
-    // 		//console.log(info)
-    // 	}
-
-    //console.log(covidData.body.data);
-  }
-});
-module.exports = minnStats;
+	console.table(minnStats)
+	return minnStats
+	}
+})
+console.log(mapdata)
+ 
