1bedbf4e99f4cf7398572ac6023366f6a2f10a93
RisenFenix <jjstram@gmail.com>
2020-04-01 21:05:12 -0500

    fillay working

--- models/example.js
+++ models/example.js
@@ -1,11 +1,11 @@
 module.exports = function (sequelize, DataTypes) {
-  var needHelpTB = sequelize.define(need_help_tb, {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    name: DataTypes.TEXT,
-    city: DataTypes.TEXT,
-    state: DataTypes.TEXT,
-    price: DataTypes.TEXT
+  var needHelpTB = sequelize.define("need_help_tb", {
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    name: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    price: DataTypes.STRING
   });
   return needHelpTB;
 };
