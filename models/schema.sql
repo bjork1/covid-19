2dce12062e02819e73010dd9cc09c4f8970a7e54
RisenFenix <jjstram@gmail.com>
2020-04-01 20:30:57 -0500

    getting new pull

--- models/schema.sql
+++ models/schema.sql
@@ -3,22 +3,13 @@ CREATE DATABASE covid19db;
 
 USE covid19db;
 
-CREATE TABLE need_help_tb
-(
-    id INT NOT NULL
-    AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR
-    (225),
-description VARCHAR
-    (255),
-city VARCHAR
-    (255),
-state VARCHAR
-    (255),
-first_name VARCHAR
-    (255),
-email VARCHAR
-    (255),
-price decimal
-    (8,2)
+CREATE TABLE need_help_tb (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(225),
+description VARCHAR(255),
+city VARCHAR (255),
+state VARCHAR (255),
+first_name VARCHAR(255),
+email VARCHAR(255),
+price decimal(8,2)
 );
