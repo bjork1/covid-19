968986eef7440f2f203254fbe7a80fed2a79f2e9
Andrew Bjork <bjorkandrew1@gmail.com>
2020-04-03 17:27:22 -0500

    update schema

--- models/schema.sql
+++ models/schema.sql
@@ -2,24 +2,22 @@ DROP DATABASE IF EXISTS covid19db;
 CREATE DATABASE covid19db;
 
 
--- USE covid19db;
+USE covid19db;
 
--- CREATE TABLE need_help_tb
--- (
- --   id INT NOT NULL
-  --  AUTO_INCREMENT PRIMARY KEY,
--- title VARCHAR
- --   (225),
--- description VARCHAR
-   -- (255),
--- city VARCHAR
-   -- (255),
--- state VARCHAR
-   -- (255),
--- first_name VARCHAR
-    -- (255),
--- email VARCHAR
-   --  (255),
--- price decimal
-   -- (8,2)
--- );
+CREATE TABLE examples
+(
+   id INT(11)
+   AUTO_INCREMENT PRIMARY KEY,
+text VARCHAR
+   (255),
+description TEXT,
+createdAt DATETIME,
+updatedAt DATETIME,
+name TEXT,
+city TEXT,
+state TEXT,
+price TEXT
+
+
+)
+
